c226b6c9f43f05e1fcb1b461160a0e92ee3e8630
David Oak <mr.davidoak@gmail.com>
2024-02-04 19:43:05 +0000

    Untrack node_modules directory

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -10,16 +10,17 @@ import {
   Paper,
   Typography,
   Button,
+  Skeleton,
 } from '@mui/material';
 import UnfollowIcon from '@mui/icons-material/RemoveCircleOutline';
-
+import ReactCountryFlag from "react-country-flag";
 
 const Follow = () => {
   const [followedStocks, setFollowedStocks] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { isAuthenticated } = useContext(AuthContext);
 
-    const handleUnfollowClick = (primaryTicker) => {
-    // This assumes `primaryTicker` is a property of each stock object in `followedStocks`
+  const handleUnfollowClick = (primaryTicker) => {
     fetch(`https://django-stocks-ecbc6bc5e208.herokuapp.com/api/stocks/${primaryTicker}/toggle_follow/`, {
         method: 'POST',
         headers: {
@@ -29,74 +30,94 @@ const Follow = () => {
     .then(response => response.json())
     .then(data => {
         if (data.status === "ok" && data.action === "unfollowed") {
-        // Remove the unfollowed stock from the list
         setFollowedStocks(followedStocks.filter(stock => stock.primary_ticker !== primaryTicker));
         } else {
-        // Handle other statuses or errors here
         console.error('Error:', data);
         }
     })
     .catch(error => {
-        // Handle network or other errors here
         console.error('Error:', error);
     });
-    };
+  };
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            fetch('https://django-stocks-ecbc6bc5e208.herokuapp.com/api/followed_stocks/', {
-                headers: {
-                    'Authorization': `Token 13502af70a55d1fcddf7c094e4418c65904ef6eb`, // Replace with the actual user's authentication token
-                },
-            })
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetch('https://django-stocks-ecbc6bc5e208.herokuapp.com/api/followed_stocks/', {
+        headers: {
+          'Authorization': `Token 13502af70a55d1fcddf7c094e4418c65904ef6eb`, // Replace with the actual user's authentication token
+        },
+      })
       .then(response => response.json())
       .then(data => {
         setFollowedStocks(data);
+        setLoading(false);
       })
       .catch(error => console.error('Error fetching followed stocks:', error));
     }
   }, [isAuthenticated]);
 
-    return (
+  return (
     <Paper sx={{ p: 2, margin: 'auto', flexGrow: 1, backgroundColor: 'transparent', boxShadow: 'none', mt: 2 }}>
-        <Typography variant="h6" gutterBottom component="div">
+      <Typography variant="h6" gutterBottom component="div">
         Stocks You're Following
-        </Typography>
-        <TableContainer component={Paper} sx={{ boxShadow: 'none' }}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead sx={{ backgroundColor: theme => theme.palette.primary.main }}>
-        <TableRow>
-          <TableCell>Stock Name</TableCell>
-          <TableCell align="right">Ticker</TableCell>
-          <TableCell align="right">Sector</TableCell>
-          <TableCell align="right">Industry</TableCell>
-          <TableCell align="right">Action</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {followedStocks.map((stock) => (
-          <TableRow key={stock.id}>
-            <TableCell component="th" scope="row">
-              {stock.name}
-            </TableCell>
-            <TableCell align="right">{stock.primary_ticker}</TableCell>
-            <TableCell align="right">{stock.sector}</TableCell>
-            <TableCell align="right">{stock.industry}</TableCell>
-            <TableCell align="right">
-              <Button
-                startIcon={<UnfollowIcon />}
-                onClick={() => handleUnfollowClick(stock.primary_ticker)}
-              >
-                Unfollow
-              </Button>
+      </Typography>
+      <TableContainer component={Paper} sx={{ boxShadow: 'none' }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: theme => theme.palette.primary.main }}>
+            <TableRow>
+              <TableCell>Stock Name</TableCell>
+              <TableCell align="right">Ticker</TableCell>
+              <TableCell align="right">Country</TableCell>
+              <TableCell align="right">Market Cap</TableCell>
+              <TableCell align="right">Industry</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading ? (
+          <TableRow>
+            <TableCell colSpan={6}>
+              <Skeleton variant="rectangular" height={200} animation="wave" />
             </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+            ) : (
+              followedStocks.map((stock) => (
+                <TableRow key={stock.id}>
+                  <TableCell component="th" scope="row">
+                    {stock.name}
+                  </TableCell>
+                  <TableCell align="right">{stock.primary_ticker}</TableCell>
+                  <TableCell align="right">
+                    {stock.country_iso && (
+                      <ReactCountryFlag
+                        countryCode={stock.country_iso}
+                        svg
+                        style={{
+                          width: '2em',
+                          height: '2em',
+                        }}
+                        title={stock.country_iso}
+                      />
+                    )}
+                  </TableCell>
+                  <TableCell align="right">{ (stock.currency_symbol) }{ (stock.highlights.market_capitalization / 1000000000).toFixed(2) }b</TableCell>
+                  <TableCell align="right">{stock.industry}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      startIcon={<UnfollowIcon />}
+                      onClick={() => handleUnfollowClick(stock.primary_ticker)}
+                    >
+                      Unfollow
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 };
 
-export default Follow; 
+export default Follow;
