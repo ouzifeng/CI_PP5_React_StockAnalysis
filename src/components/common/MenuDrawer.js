a6161d2e5634758857b36da5dfbbf4d96cb97ecd
David Oak <mr.davidoak@gmail.com>
2024-02-07 11:16:19 +0000

    change global font to open sans

--- src/components/common/MenuDrawer.js
+++ src/components/common/MenuDrawer.js
@@ -4,19 +4,44 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
 
 const MenuDrawer = ({ isOpen, onDrawerClose }) => {
+  const menuItems = [
+    { text: 'Home', link: '/' },
+    { text: 'Dividend Screener', link: '/dividend-screener' },
+    // Add other menu items here
+  ];
+
   const list = (
     <Box
-      sx={{ width: 250 }}
+      sx={{
+        width: 250,
+        '& .MuiDrawer-paper': { 
+          boxSizing: 'border-box',
+          width: 250
+        },
+      }}
       role="presentation"
       onClick={onDrawerClose}
-      onKeyDown={(event) => event.key === 'Tab' || event.key === 'Shift' ? null : onDrawerClose()}
+      onKeyDown={(event) => (event.key === 'Tab' || event.key === 'Shift') ? null : onDrawerClose()}
     >
       <List>
-        {['Home', 'Action 1', 'Action 2', 'Action 3'].map((text, index) => (
-          <ListItem button key={index} onClick={onDrawerClose}>
-            <ListItemText primary={text} />
+        {menuItems.map((item, index) => (
+          <ListItem button key={index} onClick={onDrawerClose} sx={{ 
+            '&:hover': { 
+              backgroundColor: 'primary.light', // change as per your theme
+              '& .MuiListItemText-primary': {
+                color: 'primary.contrastText',
+              },
+            },
+            '& .MuiListItemText-primary': {
+              fontWeight: 'medium', // Adjust as per your theme
+            },
+          }}>
+            <Link to={item.link} style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}>
+              <ListItemText primary={item.text} />
+            </Link>
           </ListItem>
         ))}
       </List>
@@ -28,6 +53,12 @@ const MenuDrawer = ({ isOpen, onDrawerClose }) => {
       anchor="left"
       open={isOpen}
       onClose={onDrawerClose}
+      sx={{
+        '& .MuiDrawer-paper': { 
+          backgroundColor: 'background.paper', // use your theme color
+          color: 'text.primary', // use your theme color
+        },
+      }}
     >
       {list}
     </Drawer>
--- src/pages/DividendScreener.js
+++ src/pages/DividendScreener.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Slider, Typography, Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@mui/material';
+import { Slider, Typography, Table, TableBody, TableCell, TableHead, TableRow, Paper, Box } from '@mui/material';
+import ReactCountryFlag from "react-country-flag";
+import { useNavigate } from 'react-router-dom';
 
 function DividendScreener() {
   const [stocks, setStocks] = useState([]);
   const [dividendThreshold, setDividendThreshold] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://django-stocks-ecbc6bc5e208.herokuapp.com/api/dividend_data');
-        setStocks(response.data);
+        setStocks(response.data.results);
       } catch (error) {
         console.error('Error fetching stocks:', error);
       }
@@ -19,17 +22,49 @@ function DividendScreener() {
     fetchData();
   }, []);
 
+    // Function to get style for Payout Ratio
+  const getPayoutRatioStyle = (ratio) => {
+    if (ratio > 100) {
+      return { color: 'red' }; // Red for high risk
+    } else if (ratio >= 60) {
+      return { color: 'orange' }; // Amber for moderate risk
+    }
+    return { color: 'green' }; // Green for low risk
+  };
+
+  // Function to get style for Dividend Growth
+  const getDividendGrowthStyle = (growth) => {
+    if (growth < 0) {
+      return { color: 'red' }; // Red for negative growth
+    } else if (growth === 0) {
+      return { color: 'orange' }; // Amber for no growth
+    }
+    return { color: 'green' }; // Green for positive growth
+  };
+
   const handleSliderChange = (event, newValue) => {
     setDividendThreshold(newValue);
   };
 
-  // Assuming dividendYield is a decimal like 0.0050 for 0.50%
-  const filteredStocks = stocks.filter(stock => (stock.dividend_yield * 100) >= dividendThreshold);
+  // Assuming dividend_yield is a decimal like 0.0050 for 0.50%
+  const filteredStocks = stocks.filter(stock => (parseFloat(stock.dividend_yield) * 100) >= dividendThreshold);
+
+  const formatMarketCap = (value) => {
+    return `${(value / 1e9).toFixed(2)}b`;
+  };
+
+  const formatPercentage = (value) => {
+    return `${(value * 100).toFixed(2)}%`;
+  };
+
+  const handleRowClick = (primaryTicker) => {
+    navigate(`/stocks/${primaryTicker}`);
+  };
 
   return (
     <div>
       <Typography id="dividend-slider" gutterBottom>
-        Dividend Yield Threshold: {dividendThreshold}%
+        Yield Filter: {dividendThreshold}%
       </Typography>
       <Slider
         value={dividendThreshold}
@@ -39,27 +74,54 @@ function DividendScreener() {
         step={0.1}
         marks
         min={0}
-        max={20} // Adjust based on your data
+        max={20}
       />
       <Paper>
         <Table>
           <TableHead>
             <TableRow>
-              {stocks[0] && Object.keys(stocks[0]).map((key) => (
-                <TableCell key={key} align={key === 'name' ? 'inherit' : 'right'}>
-                  {key.replace(/_/g, ' ').toUpperCase()}
-                </TableCell>
-              ))}
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Market Cap</TableCell>
+              <TableCell align="right">PE Ratio</TableCell>
+              <TableCell align="right">Dividend Yield</TableCell>
+              <TableCell align="right">Forward Yield</TableCell>
+              <TableCell align="right">Payout Ratio</TableCell>
+              <TableCell align="right">Div 5 year Growth rate</TableCell>
+              <TableCell align="right">Div Date</TableCell>              
             </TableRow>
           </TableHead>
           <TableBody>
             {filteredStocks.map((stock, index) => (
-              <TableRow key={index}>
-                {Object.entries(stock).map(([key, value]) => (
-                  <TableCell key={key} align={key === 'name' ? 'inherit' : 'right'}>
-                    {typeof value === 'number' ? value.toLocaleString() : value}
-                  </TableCell>
-                ))}
+              <TableRow key={index} hover style={{ cursor: 'pointer' }} onClick={() => handleRowClick(stock.primary_ticker)}>
+                <TableCell component="th" scope="row">
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <ReactCountryFlag
+                      countryCode={stock.country_iso}
+                      svg
+                      style={{
+                        width: '2em',
+                        height: '2em',
+                        marginRight: '0.5em', // Add some space between the flag and the text
+                      }}
+                      title={stock.country_iso}
+                    />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant="body1">{stock.name}</Typography>
+                      <Typography variant="body2" color="textSecondary">{stock.primary_ticker} - {stock.exchange}</Typography>
+                    </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align="right">{stock.currency_symbol	}{formatMarketCap(stock.market_capitalization)}</TableCell>
+                <TableCell align="right">{parseFloat(stock.pe_ratio).toFixed(2)}</TableCell>
+                <TableCell align="right">{formatPercentage(stock.dividend_yield)}</TableCell>
+                <TableCell align="right">{formatPercentage(stock.forward_annual_dividend_yield)}</TableCell>
+          <TableCell align="right" style={getPayoutRatioStyle(stock.payout_ratio)}>
+            {Number(stock.payout_ratio).toFixed(2)}%
+          </TableCell>
+          <TableCell align="right" style={getDividendGrowthStyle(stock.cagr_5_years)}>
+             {Number(stock.cagr_5_years).toFixed(2)}%
+          </TableCell>
+                <TableCell align="right">{stock.dividend_date}</TableCell>                
               </TableRow>
             ))}
           </TableBody>
