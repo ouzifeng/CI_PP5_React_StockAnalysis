37343e1494c5ec700b1d8db0a54e327de9aac843
David Oak <mr.davidoak@gmail.com>
2024-06-28 15:31:48 +0100

    fix missing crsf token in pw reset

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { TextField, Button, Typography, Box, Paper } from "@mui/material";
-import { API_URL, AUTHORIZATION_TOKEN } from '../config';
+import { API_URL } from '../config';
 
 const ForgotPassword = () => {
     // State variables for managing form data, submission status, and feedback messages
@@ -9,6 +9,22 @@ const ForgotPassword = () => {
     const [message, setMessage] = useState('');
     const [isError, setIsError] = useState(false);
     const [submitting, setSubmitting] = useState(false);
+
+    // Utility function to get CSRF token from cookies
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
 
     // Handler for form submission
     const handleSubmit = (e) => {
@@ -18,12 +34,16 @@ const ForgotPassword = () => {
         // URL of the password reset endpoint
         const apiUrl = `${API_URL}/api/user/password_reset/`;
 
-        // Configuration for the Axios request, including authorization token
+        // Get the CSRF token from the cookies
+        const csrfToken = getCookie('csrftoken');
+
+        // Configuration for the Axios request, including CSRF token
         const config = {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Token ${AUTHORIZATION_TOKEN}`,
-            }
+                'X-CSRFToken': csrfToken,
+            },
+            withCredentials: true, // Include credentials (cookies) in the request
         };
 
         // Data to be sent in the request
@@ -63,7 +83,7 @@ const ForgotPassword = () => {
                         margin="normal"
                         required
                     />
-                    <Button variant="contained" type="submit" disabled={submitting}>
+                    <Button variant="contained" type="submit" disabled={submitting} fullWidth>
                         {submitting ? 'Sending...' : 'Send Reset Link'}
                     </Button>
                     {message && (
